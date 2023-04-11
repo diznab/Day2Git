c26677960ec3b33f756e20cddc70ea9f6f75107d
diznab <129802878+diznab@users.noreply.github.com>
2023-04-10 18:07:29 -0700

    Update Day2Deno.java
    new

--- src/day2/Day2Deno.java
+++ src/day2/Day2Deno.java
@@ -3,6 +3,7 @@ package day2;
 public class Day2Deno {
 	public static void main(String[] args) {
 		System.out.println("Da 2 Git");
+		int num =10;
 	}
 /**
 This is coming from another team member 
